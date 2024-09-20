210400e08ab334ef7aa8877095d3ba3667fe843e
riosq <quentin.rios-serra@etu.umontpellier.fr>
2024-09-20 11:43:20 +0200

    flash messages added

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,6 +14,11 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function homepage(): Response
     {
+        $this->addFlash('success', 'Ceci est un message à succès !');
+        $this->addFlash('error', 'Ceci est un message d\'erreur !');
+        $this->addFlash('info', 'Ceci est un message d\'information !');
+        $this->addFlash('warning', 'Ceci est un message d\'avertissement !');
+
         $user = new User();
         $signInForm = $this->createForm(SignInType::class, $user, [
             'method' => 'POST',
