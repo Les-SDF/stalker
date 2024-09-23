30b9a05a1545fc2dd5ad0cc096961704587d5493
riosq <quentin.rios-serra@etu.umontpellier.fr>
2024-09-23 19:41:54 +0200

    fix: "other" gender replaced

--- src/Enum/Gender.php
+++ src/Enum/Gender.php
@@ -4,9 +4,384 @@ namespace App\Enum;
 
 enum Gender: string
 {
-    case Unspecified = 'unspecified';
-    case Male = 'male';
+    /**
+     * A person who does not identify themselves with or experience any gender. Agender people are also called
+     * null-gender, genderless, gendervoid, or neutral gender.
+     */
+    case Agender = 'agender';
+
+    /**
+     * Associated with being profound, deep, and infinite. The term abimegender may be used alone or in combination
+     * with other genders.
+     */
+    case Abimegender = 'abimegender';
+
+    /**
+     * A gender that is indefinable or indomitable. People identifying with this gender refuse to be categorized in
+     * any particular gender identity.
+     */
+    case AdamasGender = 'adamas_gender';
+
+    /**
+     * Also called evaisgender, this gender identity changes according to one’s surroundings.
+     */
+    case Aerogender = 'aerogender';
+
+    /**
+     * Also called aesthetgender, it is a type of gender identity derived from aesthetics.
+     */
+    case Aesthetigender = 'aesthetigender';
+
+    /**
+     * This is based on the person’s mood swings or fluctuations.
+     */
+    case Affectugender = 'affectugender';
+
+    /**
+     * A person with this gender identity is mostly agender with brief shifts of belonging to other gender types.
+     */
+    case Agenderflux = 'agenderflux';
+
+    /**
+     * The person has a fluid gender identity between more than one type of gender although they cannot name the
+     * genders they feel fluid in.
+     */
+    case Alexigender = 'alexigender';
+
+    /**
+     * This gender identity stands apart from existing social gender constructs. It means having a strong specific
+     * gender identity that is neither male nor female.
+     */
+    case Aliusgender = 'aliusgender';
+
+    /**
+     * Having a gender identity that changes depending on the person one is emotionally attached to.
+     */
+    case Amaregender = 'amaregender';
+
+    /**
+     * Having two specific gender identities simultaneously without any fluidity or fluctuations.
+     */
+    case Ambigender = 'ambigender';
+
+    /**
+     * The person identifies themselves as both man and woman and yet does not belong to either.
+     */
+    case Ambonec = 'ambonec';
+
+    /**
+     * A gender-fluid identity where a person changes their gender depending on the friends they have.
+     */
+    case Amicagender = 'amicagender';
+
+    /**
+     * A person feels a combination of feminine and masculine genders.
+     */
+    case Androgyne = 'androgyne';
+
+    /**
+     * The person feels close to a specific type of gender despite being more comfortable in closely identifying
+     * themselves with another gender.
+     */
+    case Anesigender = 'anesigender';
+
+    /**
+     * The person desires to be without any primary sexual characteristics although they do not identify themselves
+     * as genderless.
+     */
+    case Angenital = 'angenital';
+
+    /**
+     * The gender identity fades in and out in intensity but always comes back to the same gendered feeling.
+     */
+    case Anogender = 'anogender';
+
+    /**
+     * The person has a gender identity but does not label it or would prefer to not have a label.
+     */
+    case Anongender = 'anongender';
+
+    /**
+     * A protean gender that can be anything but is formless and motionless.
+     */
+    case Antegender = 'antegender';
+
+    /**
+     * This gender identity has anxiety as its prominent characteristic.
+     */
+    case Anxiegender = 'anxiegender';
+
+    /**
+     * The person has apathy or a lack of feelings toward one's gender identity.
+     */
+    case Apagender = 'apagender';
+
+    /**
+     * It means knowing what are not the characteristics of gender but not knowing what are its characteristics.
+     * Thus, a person hides its primary characteristics from the individual.
+     */
+    case Apconsugender = 'apconsugender';
+
+    /**
+     * The person has a bright and celestial gender identity.
+     */
+    case Astergender = 'astergender';
+
+    /**
+     * Having a gender identity that feels to be related to space.
+     */
+    case AstralGender = 'astral_gender';
+
+    /**
+     * Having a gender identity that feels to be closely related to being autistic.
+     */
+    case Autigender = 'autigender';
+
+    /**
+     * Having a gender experience that is deeply connected and personal to oneself.
+     */
+    case Autogender = 'autogender';
+
+    /**
+     * A gender identity that is between the two extremes of agender and any other type of gender. Both the genders
+     * are experienced one at a time without any overlapping. The two genders are described as on the opposite ends
+     * of an axis.
+     */
+    case Axigender = 'axigender';
+
+    /**
+     * Having two gender identities at the same or different times.
+     */
+    case Bigender = 'bigender';
+
+    /**
+     * Having a gender that is closely related to nature.
+     */
+    case Biogender = 'biogender';
+
+    /**
+     * Also called gender fuss, blurgender means having more than one gender identities that blur into each other so
+     * that no particular type of gender identity is clear.
+     */
+    case Blurgender = 'blurgender';
+
+    /**
+     * The person identifies themselves as male, but they experience varying degrees of male identity. This may range
+     * from feeling agender to completely male.
+     */
+    case Boyflux = 'boyflux';
+
+    /**
+     * Frequent bursts of intense feelings quickly move to the initial calm stage.
+     */
+    case Burstgender = 'burstgender';
+
+    /**
+     * This gender identity shares the qualities or aesthetics of outer space.
+     */
+    case Caelgender = 'caelgender';
+
+    /**
+     * It is associated with the feelings of considering the gender irrelevant or unimportant.
+     */
+    case Cassgender = 'cassgender';
+
+    /**
+     * There is a fluctuating intensity of irrelevance toward gender.
+     */
+    case Cassflux = 'cassflux';
+
+    /**
+     * The person feels close to one gender when depressed and to another when not depressed.
+     */
+    case Cavusgender = 'cavusgender';
+
+    /**
+     * The gender identity changes from one gender to its opposite.
+     */
+    case Cendgender = 'cendgender';
+
+    /**
+     * It is a nonbinary gender where the person has a specific masculine, feminine or neutral feelings.
+     */
+    case Ceterogender = 'ceterogender';
+
+    /**
+     * Although the person is a ceterogender, their identity keeps fluctuating between different genders.
+     */
+    case Ceterofluid = 'ceterofluid';
+
+    /**
+     * Being closely related to the gender assigned at birth during the entire life.
+     */
+    case Cisgender = 'cisgender';
+
+    /**
+     * The person’s gender cannot be comprehended or understood due to depersonalization and derealization disorder.
+     */
+    case Cloudgender = 'cloudgender';
+
+    /**
+     * Various genders are present at the same time in the individual.
+     */
+    case Collgender = 'collgender';
+
+    /**
+     * In this category, colors are used to describe gender, for example, pink gender or black gender.
+     */
+    case Colorgender = 'colorgender';
+
+    /**
+     * The person knows that they are not cisgender yet continues to identify as one for a while.
+     */
+    case Commogender = 'commogender';
+
+    /**
+     * The person feels their gender only under specific circumstances.
+     */
+    case Condigender = 'condigender';
+
+    /**
+     * Associated with the feeling of having multiple genders but preferring one over the other.
+     */
+    case Deliciagender = 'deliciagender';
+
+    /**
+     * Having multiple genders, some fluid while others are static.
+     */
+    case Demifluid = 'demifluid';
+
+    /**
+     * A combination of multiple genders with some genders static, whereas others fluctuating in intensity.
+     */
+    case Demiflux = 'demiflux';
+
+    /**
+     * The individual has partial traits of one gender and the rest of the other gender.
+     */
+    case Demigender = 'demigender';
+
+    /**
+     * The individual has multiple genders with one dominating over the rest.
+     */
+    case Domgender = 'domgender';
+
+    /**
+     * Having more than one gender with one lasting longer than the others.
+     */
+    case Duragender = 'duragender';
+
+    /**
+     * It is a personal type of gender identified by the individual alone. It is based on the person’s experience
+     * within the self.
+     */
+    case Egogender = 'egogender';
+
+    /**
+     * It is associated with a strong feeling of not being able to relate to any of the two genders of the binary
+     * gender or both of the binary gender characteristics.
+     */
+    case Epicene = 'epicene';
+
+    /**
+     * The individual relates their gender identity with spirits.
+     */
+    case Esspigender = 'esspigender';
+
+    /**
+     * The denial to identify with any gender on the gender spectrum.
+     */
+    case Exgender = 'exgender';
+
+    /**
+     * The person’s gender identity exists only when they make conscious efforts to realize it.
+     */
+    case Existigender = 'existigender';
+
     case Female = 'female';
-    case Other = 'other';
+
+    /**
+     * The person is fluid or fluctuating regarding the feminine genders.
+     */
+    case Femfluid = 'femfluid';
+
+    /**
+     * A nonbinary gender identity that is feminine.
+     */
+    case Femgender = 'femgender';
+
+    /**
+     * It means to be fluid between two or more genders with a fluctuation in the intensity of those genders.
+     */
+    case Fluidflux = 'fluidflux';
+
+    /**
+     * The person has two genders that are opposite yet they flux and work together.
+     */
+    case Gemigender = 'gemigender';
+
+    /**
+     * It is closely related to a blank space.
+     */
+    case Genderblank = 'genderblank';
+
+    /**
+     * The gender identity is fluid between infinite feelings.
+     */
+    case Genderflow = 'genderflow';
+
+    /**
+     * The person does not consistently adhere to one fixed gender and may have many genders.
+     */
+    case Genderfluid = 'genderfluid';
+
+    /**
+     * More than one gender is blurred together.
+     */
+    case Genderfuzz = 'genderfuzz';
+
+    /**
+     * The gender fluctuates in intensity.
+     */
+    case Genderflux = 'genderflux';
+
+    /**
+     * The person resists to fit in societal norms concerning genders.
+     */
+    case Genderpuck = 'genderpuck';
+
+    /**
+     * The individual blurs the preconceived boundaries of gender in relation to the gender binary or having just
+     * one gender type.
+     */
+    case Genderqueer = 'genderqueer';
+
+    /**
+     * The person is inclined toward the notion of having one gender but does not know which.
+     */
+    case GenderWitched = 'gender_witched';
+
+    /**
+     * The individual identifies themselves as a female but with varying intensities of female identities.
+     */
+    case Girlflux = 'girlflux';
+
+    /**
+     * A gender identity that gives the person peace, calm, and positivity.
+     */
+    case Healgender = 'healgender';
+
+    case Male = 'male';
+
+    /**
+     * Changing one's gender type based on the people surrounding.
+     */
+    case Mirrorgender = 'mirrorgender';
+
+    /**
+     * Having or experiencing all genders.
+     */
+    case Omnigender = 'omnigender';
+
     case PreferNotToSay = 'prefer_not_to_say';
 }
