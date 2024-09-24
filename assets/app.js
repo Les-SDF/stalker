3edc048537bcf4b50a1c2b5dc679668c8ce9c404
tueurDeDragon <ramnikhil45@gmail.com>
2024-09-24 20:17:14 +0200

    add : de la fonction checkCodeProfile dans app.js WIP

--- assets/app.js
+++ assets/app.js
@@ -10,6 +10,7 @@ import './bootstrap';
 import 'flowbite';
 
 import { createIcons, icons } from 'lucide';
+import {Routing} from "../public/bundles/fosjsrouting/js/router";
 
 // Initialize Lucide
 document.addEventListener('DOMContentLoaded', () => {
@@ -17,6 +18,28 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // TODO: Faire la méthode de vérification de la disponibilité d'un code de profil en appelant la route check_profile_code_availability
+
+function checkCodeProfile(code){
+    let url = Routing.generate('check_profile_code_availability');
+    let data = {
+        profileCode: code
+    }
+    fetch(url,{
+            method: 'POST',
+            body: JSON.stringify(data)
+        }
+    ).then(datas => {
+        if (datas['is_available']){
+            return true;
+        }
+    }).catch(
+      error => {
+          console.error("Erreur : ", error);
+      }
+    );
+    return false
+}
+
 /**
  * La route doit être appeler en POST et demande un paramètre profileCode
  * Voici ce que la route retourne en JSON :
