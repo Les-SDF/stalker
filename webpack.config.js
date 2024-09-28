5045e8e0c560bd6d85745cd25d0b7ecf9d01da19
Poirrier Peter <peterdesete@gmail.com>
2024-09-28 11:07:04 +0200

    Modification code user fonctionnel

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    // .addEntry('script', './public/js/script.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
